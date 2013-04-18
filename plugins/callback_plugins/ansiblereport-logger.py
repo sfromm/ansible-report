9f8249331077880464a34f4b7a80373bfd98fb0e
Stephen Fromm <stephenf@nero.net>
2013-04-18 12:19:57 -0700

    Fix copyright/license info

--- plugins/callback_plugins/ansiblereport-logger.py
+++ plugins/callback_plugins/ansiblereport-logger.py
@@ -1,13 +1,14 @@
-# (C) 2013, Stephen Fromm, <sfromm@gmail.com>
+# Written by Stephen Fromm <sfromm@gmail.com>
+# (C) 2013 University of Oregon
 
-# This file is part of Ansible
+# This file is part of ansible-report
 #
-# Ansible is free software: you can redistribute it and/or modify
+# ansible-report is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Ansible is distributed in the hope that it will be useful,
+# ansible-report is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
