926798f32708928f2921b7815901eb0d3e698c84
Stephen Fromm <stephenf@nero.net>
2013-05-09 10:01:31 -0700

    change default smtp subject

--- lib/ansiblereport/constants.py
+++ lib/ansiblereport/constants.py
@@ -31,8 +31,7 @@ DEFAULT_TASK_RESULTS.extend(DEFAULT_TASK_OKAY_RESULTS)
 DEFAULT_TASK_RESULTS.extend(DEFAULT_TASK_WARN_RESULTS)
 
 DEFAULT_SMTP_SERVER = 'localhost'
-DEFAULT_SMTP_SUBJECT = 'ansible-report: {0}'.format(
-        datetime.datetime.now().strftime(DEFAULT_FRIENDLY_STRFTIME))
+DEFAULT_SMTP_SUBJECT = 'ansible-report'
 DEFAULT_SMTP_SENDER = 'nobody@{0}'.format(socket.getfqdn())
 DEFAULT_SMTP_RECIPIENT = 'root@{0}'.format(socket.getfqdn())
 
