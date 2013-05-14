4648c1a4bd4662833dfcd52f293bfc6b93f963b8
Stephen Fromm <stephenf@nero.net>
2013-05-14 15:57:12 -0700

    updates to constants.py
    Take in get_config_value() from utils.py.  Update handling of SMTP
    constants and add DEFAULT_OUTPUT_PLUGIN_PATH.

--- lib/ansiblereport/constants.py
+++ lib/ansiblereport/constants.py
@@ -14,10 +14,23 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# along with ansible-report.  If not, see <http://www.gnu.org/licenses/>.
 
 import datetime
 import socket
+
+import ansible.constants as AC
+
+def get_config_value(key, env_var, default):
+    ''' Look up key in ansible.cfg
+    This uses load_config_file() and get_config() from ansible.constants
+    '''
+    config = AC.load_config_file()
+    return AC.get_config(config, DEFAULT_SECTION, key, env_var, default)
+
+DEFAULT_SECTION = 'ansiblereport'
+
+DEFAULT_VERBOSE = False
 
 DEFAULT_STRFTIME = '%Y-%m-%d %H:%M:%S'
 DEFAULT_SHORT_STRFTIME = '%H:%M:%S'
@@ -30,10 +43,12 @@ DEFAULT_TASK_RESULTS = []
 DEFAULT_TASK_RESULTS.extend(DEFAULT_TASK_OKAY_RESULTS)
 DEFAULT_TASK_RESULTS.extend(DEFAULT_TASK_WARN_RESULTS)
 
-DEFAULT_SMTP_SERVER = 'localhost'
-DEFAULT_SMTP_SUBJECT = 'ansible-report'
-DEFAULT_SMTP_SENDER = 'nobody@{0}'.format(socket.getfqdn())
-DEFAULT_SMTP_RECIPIENT = 'root@{0}'.format(socket.getfqdn())
+DEFAULT_SMTP_SERVER = get_config_value('smtp.server', None, 'localhost')
+DEFAULT_SMTP_SUBJECT = get_config_value('smtp.subject', None, 'ansible-report')
+DEFAULT_SMTP_SENDER = get_config_value('smtp.sender', None, 'nobody@{0}'.format(socket.getfqdn()))
+DEFAULT_SMTP_RECIPIENT = get_config_value('smtp.recipient', None, 'root@{0}'.format(socket.getfqdn()))
 
 DEFAULT_OUTPUT = ['screen']
-DEFAULT_OUTPUTS_AVAIL = ['screen', 'email']
+DEFAULT_OUTPUT_PLUGIN_PATH = AC.shell_expand_path(
+        get_config_value('output_plugins',
+            'ANSIBLEREPORT_OUTPUT_PLUGINS', '/usr/share/ansible-report/plugins'))
