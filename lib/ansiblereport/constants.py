dae0994601bc028b522a4574f6db9566c3c0167c
Stephen Fromm <stephenf@nero.net>
2013-07-24 23:10:42 -0700

    drop use of socket when setting default for smtp_sender and smtp_recipient

--- lib/ansiblereport/constants.py
+++ lib/ansiblereport/constants.py
@@ -17,7 +17,6 @@
 # along with ansible-report.  If not, see <http://www.gnu.org/licenses/>.
 
 import datetime
-import socket
 
 import ansible.constants as AC
 
@@ -51,8 +50,8 @@ DEFAULT_TASK_RESULTS.extend(DEFAULT_TASK_WARN_RESULTS)
 
 DEFAULT_SMTP_SERVER = get_config_value('smtp.server', None, 'localhost')
 DEFAULT_SMTP_SUBJECT = get_config_value('smtp.subject', None, 'ansible-report')
-DEFAULT_SMTP_SENDER = get_config_value('smtp.sender', None, 'nobody@{0}'.format(socket.getfqdn()))
-DEFAULT_SMTP_RECIPIENT = get_config_value('smtp.recipient', None, 'root@{0}'.format(socket.getfqdn()))
+DEFAULT_SMTP_SENDER = get_config_value('smtp.sender', None, 'nobody@localhost')
+DEFAULT_SMTP_RECIPIENT = get_config_value('smtp.recipient', None, 'root@localhost')
 
 DEFAULT_OUTPUT = ['screen']
 DEFAULT_OUTPUT_PLUGIN_PATH = AC.shell_expand_path(
