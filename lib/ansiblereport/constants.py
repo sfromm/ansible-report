5a6b8b538a47555342b1427ee33beac1331fe5ba
Stephen Fromm <stephenf@nero.net>
2014-02-04 15:54:58 -0800

    tweak name of environment variable to specify sqlite uri

--- lib/ansiblereport/constants.py
+++ lib/ansiblereport/constants.py
@@ -35,7 +35,7 @@ DEFAULT_BACKOFF_START = get_config_value('backoff.start', None, 0.5)
 DEFAULT_BACKOFF_MULT = get_config_value('backoff.mult', None, 1.05)
 DEFAULT_BACKOFF_MAX = get_config_value('backoff.max', None, 60)
 
-DEFAULT_DB_URI = get_config_value('sqlalchemy.url', 'ANSIBLE_REPORT_DB_URI', 'sqlite://')
+DEFAULT_DB_URI = get_config_value('sqlalchemy.url', 'ANSIBLEREPORT_DB_URI', 'sqlite://')
 
 DEFAULT_STRFTIME = '%Y-%m-%d %H:%M:%S'
 DEFAULT_SHORT_STRFTIME = '%H:%M:%S'
