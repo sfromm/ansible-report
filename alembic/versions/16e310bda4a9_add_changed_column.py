37029c742217223fc707f412677aa67581fd0582
Stephen Fromm <stephenf@nero.net>
2014-02-24 20:24:40 -0800

    populate changed column during migration

--- alembic/versions/16e310bda4a9_add_changed_column.py
+++ alembic/versions/16e310bda4a9_add_changed_column.py
@@ -12,13 +12,35 @@ down_revision = '3736909be4d5'
 
 from alembic import op
 import sqlalchemy as sa
+import json
 
+taskhelper = sa.Table(
+    'task',
+    sa.MetaData(),
+    sa.Column('id', sa.Integer()),
+    sa.Column('hostname', sa.String()),
+    sa.Column('module', sa.String()),
+    sa.Column('result', sa.String()),
+    sa.Column('data', sa.Text()),
+    sa.Column('changed', sa.Boolean()),
+    sa.Column('playbook_id', sa.Integer())
+)
 
 def upgrade():
     op.add_column('task',
                   sa.Column('changed', sa.Boolean, nullable=True))
     op.create_index('task_changed_idx', 'task', ['changed'])
-
+    connection = op.get_bind()
+    for task in connection.execute(taskhelper.select()):
+        data = json.loads(task.data)
+        changed = False
+        if 'changed' in data:
+            changed = data['changed']
+        connection.execute(
+            taskhelper.update().where(
+                taskhelper.columns.id == task.id
+            ).values(changed=changed)
+        )
 
 def downgrade():
     op.drop_column('task', 'changed')
