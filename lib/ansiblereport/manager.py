fb01b5559a645787d95cdbcd9186870af58b4310
Stephen Fromm <stephenf@nero.net>
2014-04-07 10:27:58 -0700

    begin work on further developing functionality of manager.py

--- lib/ansiblereport/manager.py
+++ lib/ansiblereport/manager.py
@@ -19,8 +19,22 @@
 import ansiblereport.constants as C
 from ansiblereport.model import *
 
+
+import functools
+import random
 import time
 import sqlalchemy
+import os
+import sys
+
+def _db_error_decorator(callable):
+    @functools.wraps(callable)
+    def _wrap(self, *args, **kwargs):
+        try:
+            return callable(self, *args, **kwargs)
+        except Exception as e:
+            raise
+    return _wrap
 
 class Manager(object):
     ''' db manager object '''
@@ -34,8 +48,23 @@ class Manager(object):
             from alembic import command
             alembic_cfg = Config(alembic_ini)
             command.stamp(alembic_cfg, 'head')
-        Session = sessionmaker(bind=self.engine)
+        Session = sessionmaker(bind=self.engine, autocommit=True)
         self.session = Session()
+
+    @_db_error_decorator
+    def save(self, model, nocommit=False):
+        ''' save an object '''
+        with self.session.begin(subtransactions=True):
+            self.session.add(model)
+            self.session.flush()
+
+    def get_or_create(self, model, **kwargs):
+        ''' get or create an object '''
+        instance = self.session.query(model).filter_by(**kwargs).first()
+        if not instance:
+            instance = model(**kwargs)
+            self.save(instance)
+        return instance
 
     # The following is based on buildbot/master/buildbot/db/pool.py
     def run(self, callable, *args, **kwargs):
@@ -46,12 +75,11 @@ class Manager(object):
                 try:
                     rv = callable(self.session, *args, **kwargs)
                     break
-                except sqlalchemy.exc.OperationalError, e:
+                except sqlalchemy.exc.OperationalError as e:
                     text = e.orig.args[0]
                     if not isinstance(text, basestring):
                         raise
                     if "Lost connection" in text \
-                        or "transaction has been rolled back" in text \
                         or "database is locked" in text:
 
                         # raise exception if have retried too often
@@ -62,9 +90,13 @@ class Manager(object):
                         # sleep and retry
                         time.sleep(backoff)
                         backoff *= C.DEFAULT_BACKOFF_MULT
+                        # try again
                         continue
                     else:
                         raise
+                except Exception as e:
+                    raise
             finally:
                 pass
         return rv
+
