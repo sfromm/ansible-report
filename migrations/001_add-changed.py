9188557d0bd7d7fdc4d6c28f72f6c9b81cc74d32
Stephen Fromm <stephenf@nero.net>
2014-05-28 13:32:05 -0700

    create/drop index for task.changed during migration

--- migrations/001_add-changed.py
+++ migrations/001_add-changed.py
@@ -15,6 +15,8 @@ def up():
         q = mgr.database.execute_sql("UPDATE task SET changed = 1 WHERE data LIKE '%changed\": true%'")
         logging.info("Updated changed column for %s tasks",
                      mgr.database.rows_affected(q))
+        q = mgr.database.execute_sql('CREATE INDEX IF NOT EXISTS "task_changed" ON "task" ("changed")')
+        logging.info("Created index task_changed")
 
 def down():
     ''' drop changed column from task '''
@@ -22,4 +24,5 @@ def down():
         mgr.database.execute_sql("CREATE TABLE task_temp AS SELECT id, hostname, module, result, timestamp, user_id, playbook_id, data FROM task")
         mgr.database.execute_sql("DROP TABLE task")
         mgr.database.execute_sql("ALTER TABLE task_temp RENAME TO task")
+        mgr.database.execute_sql('DROP INDEX IF EXISTS "task_changed"')
         mgr.database.execute_sql("VACUUM")
